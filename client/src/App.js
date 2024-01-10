2468ca8de628bb49d08c8dc78608eed55eacee7e
Vinayak <myselfvinayakhegde@gmail.com>
2024-01-10 12:26:00 +0530

    feat: implement basic implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,79 @@
 import "./App.css";
 
 function App() {
+  const amount = 500;
+  const currency = "INR";
+  const receiptId = "qwsaq1";
+
+  console.log(process.env.REACT_APP_RAZORPAY_KEY_ID);
+
+  const paymentHandler = async (e) => {
+    const response = await fetch("http://localhost:5001/order", {
+      method: "POST",
+      body: JSON.stringify({
+        amount,
+        currency,
+        receipt: receiptId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const order = await response.json();
+    console.log(order);
+
+    var options = {
+      key: process.env.REACT_APP_RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
+      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency,
+      name: "Acme Corp", //your business name
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: async function (response) {
+        const body = {
+          ...response,
+        };
+
+        const validateRes = await fetch(
+          "http://localhost:5001/order/validate",
+          {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const jsonRes = await validateRes.json();
+        console.log(jsonRes);
+      },
+      prefill: {
+        //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+        name: "Thala", //your customer's name
+        email: "thala@example.com",
+        contact: "7777777777", //Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      console.log(response.error);
+    });
+    rzp1.open();
+    e.preventDefault();
+  };
+
+
   return (
-    <div>
+    <div className="app">
       <h1>Razorpay Integration</h1>
+      <button onClick={paymentHandler}>Pay</button>
     </div>
   );
 }
